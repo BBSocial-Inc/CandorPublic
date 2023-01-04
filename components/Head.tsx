f7084f357a617474001ad7dd05a9adc529ef5010
bilyhack12 <ibrahim.bileri.ib@gmail.com>
2023-01-04 08:53:27 +0000

    added a test file

---
+++ components/Head.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+
+export default function Headd({ title, image }:any) {
+  const dash = title == "" ? "" : " |";
+  return (
+    <Head>
+      <title>{title + dash + " Discover and Manage Events"} </title>
+      <meta
+        name="title"
+        content={title + dash + " Discover and Manage Events"}
+      ></meta>
+      <meta
+        name="og:title"
+        content={title + dash + " Discover and Manage Events"}
+      ></meta>
+
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content="https://tease.africa/"></meta>
+      <meta
+        name="description"
+        content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
+      />
+      <meta
+        name="og:description"
+        content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
+      />
+      <link rel="icon" href="/icon.png" />
+      <meta
+        property="og:image"
+        content={image ? image : "https://teaseafrica.netlify.app/1.png"}
+      ></meta>
+
+      <meta
+        name="twitter:title"
+        content={title + dash + " Discover and Manage Events"}
+      ></meta>
+      <meta
+        content="With Tease you can discover events in your city, buy tickets to music or games, create and manage your own event and sell tickets to other users."
+        name="twitter:description"
+      ></meta>
+      <meta name="twitter:url" content="https://tease.africa/"></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:site" content="@TeaseAfrica"></meta>
+      <meta name="twitter:creator" content="@TeaseAfrica"></meta>
+      <meta
+        name="twitter:image"
+        content={image ? image : "https://teaseafrica.netlify.app/1.png"}
+      ></meta>
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y5P4TVP212"
+      ></script>
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `    window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+        
+            gtag('config', 'G-Y5P4TVP212')`,
+        }}
+      />
+    </Head>
+  );
+}
