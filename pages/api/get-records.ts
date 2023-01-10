c9f1cd6673efc4e447b1fae283042faf8bd3a5a8
bilyhack12 <ibrahim.bileri.ib@gmail.com>
2023-01-10 18:45:39 +0000

    fixed build error

--- pages/api/get-records.ts
+++ pages/api/get-records.ts
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { getMinifiedItem, marketingTable } from "../../utils/airtable";
+import { getMinifiedItem, isComingTable } from "../../utils/airtable";
 
 export default async (req: any, res: any) => {
 	try {
-		const records = await marketingTable.select({}).all();
+		const records = await isComingTable.select({}).all();
 		const formattedRecords = records.map(getMinifiedItem);
 		//if(formattedRecords.length>0)
 		res.status(200).json(formattedRecords);
